d2aabee574e75ccfdc8fcec1284622a2410d2f0e
Ehjyfcxbm <sukit.inp@dome.tu.ac.th>
2024-10-31 12:02:53 +0700

    Update pygame template.py

--- pygame template.py
+++ pygame template.py
@@ -5,14 +5,13 @@ WIDTH = 360
 HEIGHT = 480
 FPS = 30
 
-# define colors
+# colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
-# initialize pygame and create window
 pygame.init()
 pygame.mixer.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
