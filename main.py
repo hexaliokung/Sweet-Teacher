251b69b9805c8e0173b2f4865a8e9356368bcef4
hexaliokung <thatphong.saikham@gmail.com>
2024-10-31 11:04:45 +0700

    initial commit

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from os import path
 from sprites import *
 from settings import *
 from tilemap1 import *
-x = 1516135123135
 
 # ------------------------------------ สร้าง class Game ขึ้น ------------------------------------
 class Game:
